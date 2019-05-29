740b8e895c792205a8928e99317fbad9defb1196
unknown <masrin123@hotmail.com>
2019-05-29 11:18:29 -0400

    json ref error fix

--- backend/server.js
+++ backend/server.js
@@ -9,16 +9,16 @@ app.use(bodyParser.json())
 
 // Knex Setup
 var knex = require('knex')({
-  client: 'pg',
-  connection: process.env.DATABASE_URL,
-  ssl: true
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    ssl: true
 });
+
 require('knex-paginator')(knex);
 const db = require("./db.js");
 
 // Initialize Database
 db.initialize(knex);
-
 
 //////// ROUTES ////////
 
@@ -28,60 +28,62 @@ app.get("/", function(req, res){
 
 // Search users
 app.get("/users", function(req, res){
-  knex.from("users")
+    knex.from("users")
     .select("*")
     .modify(function(queryBuilder){
-      if("name" in req.query) queryBuilder.where('search_body', 'like', '%' + req.query.name.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").toLowerCase() + '%');
-      if("id" in req.query) queryBuilder.where('facebook_id', req.query.id);
+        if("name" in req.query) queryBuilder.where('search_body', 'like', '%' + req.query.name.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").toLowerCase() + '%');
+        if("id" in req.query) queryBuilder.where('facebook_id', req.query.id);
     })
     .paginate(30, req.query.page ? parseInt(req.query.page) : 1) //Paginate
     .then(function(results){
-      res.json(results);
+        res.json(results);
     })
     .catch(function(e){
-      console.log(e);
-      res.status(500);
-      res.send("ERROR:" + e);
+        console.log(e);
+        res.status(500);
+        res.send("ERROR:" + e);
     });
 });
 
 // Search Plans
 app.get("/plans", function(req, res){
-  knex.from("plans")
+    knex.from("plans")
     .select("*")
     .modify(function(queryBuilder){
-      if("id" in req.query) queryBuilder.where('facebook_id', req.query.id);
+        if("id" in req.query) queryBuilder.where('facebook_id', req.query.id);
     })
     .paginate(30, req.query.page ? parseInt(req.query.page) : 1) //Paginate
     .then(function(results){
-      res.json(results);
+        res.json(results);
     })
     .catch(function(e){
-      console.log(e);
-      res.status(500);
-      res.send("ERROR");
+        console.log(e);
+        res.status(500);
+        res.send("ERROR");
     });
 });
 
 
 // Create users
 app.post("/users", function(req, res) {
-  knex("select").select().where("facebook_id", req.body.facebook_id).then((response) => {
-    if (!response.length){ // Check if the username doesn't already exist
-      knex("users").insert({
-        facebook_id: req.body.facebook_id,
-        name: req.body.name
-      }).then(function() {
-        res.status(200).send("successfully added user to database");
-      })
+    console.log(req.body);
+    knex("select").select().where("facebook_id", req.body.facebook_id).then((response) => {
+        if (!response.length){ // Check if the username doesn't already exist
+        knex("users").insert({
+            facebook_id: req.body.facebook_id,
+            name: req.body.name
+        }).then(function() {
+            res.status(200).send("successfully added user to database");
+        })
     } else {
-      res.status(500).send("error");
+        res.status(500).send("error");
     }
-  })
+})
 });
 
 // Create plan
 app.post("/plans", function(req, res) {
+    console.log(req.body)
     knex("plans").insert({
         facebook_id: req.body.facebook_id,
         location_name: req.body.location_name,
@@ -95,5 +97,5 @@ app.post("/plans", function(req, res) {
 
 // Run Server
 app.listen(process.env.PORT || 3000, function(){
-  console.log("Listening on port " + (process.env.PORT || 3000));
+    console.log("Listening on port " + (process.env.PORT || 3000));
 });
